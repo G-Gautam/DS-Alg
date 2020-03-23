5ef725866cc1adcccae59b96500b5730036cc28d
Gautam <gautamgupta9298@gmail.com>
2020-03-23 13:17:03 -0400

    Array rotation recursion

--- Arrays/array_rotation.py
+++ Arrays/array_rotation.py
@@ -1,10 +1,9 @@
-import pytest
-
 #ar: the array to be rotated
 #d: the number of rotations
 #n: the size of the array
 
 #Output: the rotated array
+
 #Time Complexity: O(n)
 #Space Complexity: O(n)
 def rotate_with_temp_array(ar, d, n):
@@ -16,3 +15,11 @@ def rotate_with_temp_array(ar, d, n):
         temp.append(ar[i])
     return temp
 
+#Time Complexity: O(n)
+#Space Complexity: O(n)
+def rotate_inplace_with_recursion(ar, d, n):
+    if(d == 0):
+        return ar
+    x = ar.pop(0)
+    ar.append(x)
+    return rotate_inplace_with_recursion(ar, d-1, n)
--- Arrays/test_array_rotation.py
+++ Arrays/test_array_rotation.py
@@ -1,6 +1,12 @@
 import array_rotation
+import pytest
 
 def test_rotate_with_temp_array():
     ar = [1,2,3,4,5,6]
     output = [3,4,5,6,1,2]
     assert array_rotation.rotate_with_temp_array(ar, 2, 6) == output
+
+def test_rotate_inplace_with_recursion():
+    ar = [1,2,3,4,5,6]
+    output = [3,4,5,6,1,2]
+    assert array_rotation.rotate_inplace_with_recursion(ar, 2, 6) == output
